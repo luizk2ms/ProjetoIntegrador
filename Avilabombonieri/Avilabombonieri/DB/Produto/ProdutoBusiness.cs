91e4597487671a8a14bc4bb80c3f80db82c819e0
win 7 <win 7@WIN7-PC>
2018-11-07 21:18:42 -0200

    produto arrumado

---
+++ Avilabombonieri/Avilabombonieri/DB/Produto/ProdutoBusiness.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avilabombonieri.DB.Produto
+{
+    class ProdutoBusiness
+     {
+        public int Salvar(ProdutoDTO dto)
+        {
+            ProdutoDatabase db = new ProdutoDatabase();
+            return db.Salvar(dto);
+        }
+
+        public void Alterar(ProdutoDTO dto)
+        {
+            ProdutoDatabase db = new ProdutoDatabase();
+            db.Alterar(dto);
+        }
+        public void Remover(int ID)
+        {
+            ProdutoDatabase db = new ProdutoDatabase();
+            db.Remover(ID);
+        }
+
+        public List<ProdutoViewDTO> Listar()
+        {
+            ProdutoDatabase db = new ProdutoDatabase();
+            return db.Listar();
+        }
+
+        public List<ProdutoViewDTO> ConsultarporNome(string nome)
+        {
+            ProdutoDatabase db = new ProdutoDatabase();
+            return db.ConsultarporNome(nome);
+        }
+
+        public List<ProdutoViewDTO> ConsultarpoId(int ID)
+        {
+            ProdutoDatabase db = new ProdutoDatabase]();
+            return db.ConsultarporId(ID);
+        }
+    }
+}
